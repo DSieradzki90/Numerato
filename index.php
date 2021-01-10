f81c0ceaaac9d26a839b567371d479ccccbb4121
Lomax <lomax31@o2.pl>
2021-01-10 14:34:15 +0100

    Second

---
+++ index.php
@@ -0,0 +1,60 @@
+
+<tbody>
+<tr>
+<td style="text-align: right;">&nbsp;</td>
+<td>ZAPIEKANKI Z PIECA</td>
+<td style="text-align: center;">mała<br>35cm</td>
+<td style="text-align: center;">duża<br>70cm</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">48.</td>
+<td>sos, pieczarki, ser</td>
+<td style="text-align: center;">6,00</td>
+<td style="text-align: center;">13,00</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">49.</td>
+<td>sos, szynka, pieczarki, ser</td>
+<td style="text-align: center;">7,00</td>
+<td style="text-align: center;">14,00</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">50.</td>
+<td>sos, kebab drobiowy, pieczarki, ser</td>
+<td style="text-align: center;">7,00</td>
+<td style="text-align: center;">14,00</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">51.</td>
+<td>sos, szynka, pieczarki, kukurydza, ser</td>
+<td style="text-align: center;">8,00</td>
+<td style="text-align: center;">15,00</td>
+</tr>
+<tr>
+<td style="text-align: right;">&nbsp;</td>
+<td>&nbsp;</td>
+<td style="text-align: center;">&nbsp;</td>
+<td style="text-align: center;">&nbsp;</td>
+</tr>
+<tr>
+<td style="text-align: right;">&nbsp;</td>
+<td>TORTILLA</td>
+<td style="text-align: center;">&nbsp;</td>
+<td style="text-align: center;">&nbsp;</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">52.</td>
+<td>Tortilla z kebabem drobiowym<br>tortilla pszenna, kebab drobiowy, ser, kapusta pekińska, pomidor, ogórek, sosy</td>
+<td style="text-align: center;" colspan="2">14,00</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">53.</td>
+<td>Tortilla na ostro<br>tortilla pszenna, ser, kebab drobiowy, kapusta&nbsp;pekińska, papryczka jalepeno, papryka, oliwki, sosy</td>
+<td style="text-align: center;" colspan="2">14,00</td>
+</tr>
+<tr id="tlo">
+<td style="text-align: right;">54.</td>
+<td>Tortilla pszenna<br>tortilla pszenna, sałata lodowa, ogórek, kapusta pekińska,&nbsp;pomidor i polędwiczki z kurczaka, sosy</td>
+<td style="text-align: center;" colspan="2">14,00</td>
+</tr>
+</tbody>
